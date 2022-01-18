b8d98cc2bc269de385db8d2301d58c5edbffadea
Michael Martin <93306701+Khalyst0@users.noreply.github.com>
2022-01-17 22:36:12 -0800

    Update Main.gd
    Added: remove all mobs at game end

--- Michael/Main.gd
+++ Michael/Main.gd
@@ -12,10 +12,16 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	
+	get_tree().call_group("mobs", "queue_free")
+	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$HUD.show_game_over()
 
 
 
@@ -33,6 +39,8 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
